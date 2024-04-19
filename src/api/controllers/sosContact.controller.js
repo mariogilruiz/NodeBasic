e082eb2854ac979e4aaeba619d2c056a7a1eca99
freelivecode <mariogilruiz66@gmail.com>
2024-04-19 21:11:50 +0200

    prieba

--- src/api/controllers/sosContact.controller.js
+++ src/api/controllers/sosContact.controller.js
@@ -4,11 +4,11 @@ const { HTTPSTATUSCODE } = require("../../utils/httpStatusCode");
 const createSosContact = async (req, res, next) => {
   try {
     const sosContact = await SosContact.create(req.body);
-    console.log (sosContact)
+    console.log(sosContact)
     res.status(201).json({
-    //   status: 201,
-    //   message: HTTPSTATUSCODE[201],
-     data: sosContact,
+      status: 201,
+      message: HTTPSTATUSCODE[201],
+      data: sosContact,
     });
   } catch (error) {
     next(error);
@@ -18,10 +18,10 @@ const createSosContact = async (req, res, next) => {
 const getAllSosContact = async (req, res, next) => {
   try {
     const sosContact = await SosContact.find().populate("user");
-    console.log (sosContact)
+    console.log(sosContact)
     res.status(200).json({
-    //   status: 200,
-    //   message: HTTPSTATUSCODE[200],
+      status: 200,
+      message: HTTPSTATUSCODE[200],
       data: sosContact,
     });
   } catch (error) {
@@ -34,8 +34,8 @@ const getSosContactById = async (req, res, next) => {
     const sosContact = await SosContact.findById(req.params.id);
     if (user) {
       res.status(200).json({
-        // status: 200,
-        // message: HTTPSTATUSCODE[200],
+        status: 200,
+        message: HTTPSTATUSCODE[200],
         data: sosContact,
       });
     } else {
@@ -56,8 +56,8 @@ const updateSosContact = async (req, res, next) => {
     });
     if (sosContact) {
       res.status(200).json({
-        // status: 200,
-        // message: HTTPSTATUSCODE[200],
+        status: 200,
+        message: HTTPSTATUSCODE[200],
         data: sosContact,
       });
     } else {
@@ -76,8 +76,8 @@ const deleteSosContact = async (req, res, next) => {
     const sosContact = await SosContact.findByIdAndDelete(req.params.id);
     if (sosContact) {
       res.status(204).json({
-         status: 204,
-        // message: HTTPSTATUSCODE[204],
+        status: 204,
+        message: HTTPSTATUSCODE[204],
         data: sosContact,
       });
     } else {
